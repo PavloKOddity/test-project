b35d816e04d1d8bfe24410f239e51b4d9a94e0f5
Pavlo Koval <pavlok@ilmakiage.com>
2024-10-22 16:30:53 +0400

    test-project update

--- src/Handlers/StorePartsHandler.ts
+++ src/Handlers/StorePartsHandler.ts
@@ -5,7 +5,7 @@ import { StatusCodes } from "http-status-codes";
 import { StorePartsRepository } from "../Repositories/StorePartsRepository";
 
 /**
- * Task:
+ * Task requirements:
  * Implement the handle method for saving data about car parts and their supplier.
  *
  * The system should enforce the following requirements:
